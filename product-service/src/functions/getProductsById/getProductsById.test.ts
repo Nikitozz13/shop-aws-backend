5890186d876981614704f1ba157de1a84ea36194
Nikita Staroseltsev <nikitozz_13@mail.ru>
2023-03-02 16:23:41 +0300

    TASK-3 Update unit tests to cover all lambda handlers

--- product-service/src/functions/getProductsById/getProductsById.test.ts
+++ product-service/src/functions/getProductsById/getProductsById.test.ts
@@ -2,7 +2,7 @@ import * as LambdaTester from "lambda-tester";
 import { main as getProductsById } from "@functions/getProductsById/handler";
 import { getProductById } from "@libs/get-data-actions";
 
-jest.mock("@libs/get-data-actions");
+jest.mock('@libs/get-data-actions');
 
 jest.mock('@libs/lambda', () => {
   return {
@@ -11,9 +11,8 @@ jest.mock('@libs/lambda', () => {
 });
 
 describe("getProductsById", () => {
-  const mockProduct = { id: "2", title: "Product 2" };
-
   test("should return needed product with status 200", async () => {
+    const mockProduct = { id: "2", title: "Product 2" };
     (getProductById as jest.Mock).mockResolvedValueOnce(mockProduct);
     await LambdaTester(getProductsById)
       .event({pathParameters: { productId: '2' }} as any)
@@ -24,12 +23,14 @@ describe("getProductsById", () => {
   });
 
   test("should return error when product was not found", async () => {
-    (getProductById as jest.Mock).mockResolvedValueOnce(null);
+    const errorMessage = 'Product not found';
+    const expectedResult = { error: errorMessage };
+    (getProductById as jest.Mock).mockImplementationOnce(() => Promise.reject(errorMessage));
     await LambdaTester(getProductsById)
       .event({pathParameters: { productId: '3' }} as any)
       .expectResult((result) => {
         expect(result.statusCode).toBe(200);
-        expect(result.body).toBe(JSON.stringify(mockProduct));
+        expect(result.body).toBe(JSON.stringify(expectedResult));
       });
   });
 });
