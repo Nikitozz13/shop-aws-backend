b6315f3ba2a2142cc2d47c1fb363b4bf87049252
Nikita Staroseltsev <nikitozz_13@mail.ru>
2023-03-23 15:06:55 +0300

    TASK-6 catalogBatchProcess should iterate over SQS messages and create products in DB

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,4 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
 import { SQSEvent, SQSHandler } from 'aws-lambda';
+import { createProductTransaction as createProductRequest } from '@libs/dynamo-service';
+import { InvalidProductDataError } from '@libs/errors';
 
 const catalogBatchProcess: SQSHandler = async (event: SQSEvent) => {
   console.log('SQSHandler:event:catalogBatchProcess', event);
@@ -6,6 +9,18 @@ const catalogBatchProcess: SQSHandler = async (event: SQSEvent) => {
   try {
     for (const record of event.Records) {
       console.log('Record: ', record);
+      
+      const { title, description = '', price, count } = JSON.parse(record.body);
+      if (!title || !description || !price || !count) {
+        throw new InvalidProductDataError(`Product data is invalid for: ${record.body}`);
+      }
+
+      const uuid = uuidv4();
+      const result = await createProductRequest(
+        { id: uuid, title, description, price },
+        { product_id: uuid, count }
+      );
+      console.log('Dynamo:result:createProductWithStock', result);
     }
 
     console.log('SQSHandler:result: function execution completed with success');
